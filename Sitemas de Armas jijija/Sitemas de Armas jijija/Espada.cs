11017d0bcc910d9f62dd9c75dede254291d56ae6
ArchieJasper <diego_Camposdacr@hotmail.com>
2023-04-04 16:26:58 -0500

    pequeño cambios

--- Sitemas de Armas jijija/Sitemas de Armas jijija/Espada.cs
+++ Sitemas de Armas jijija/Sitemas de Armas jijija/Espada.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace Sitemas_de_Armas_jijija
 {
-    class Espada: Inventario
+    class Espada: Armas&Munición
     {
         private string Nombre;
         private float CostoEspada;
@@ -26,11 +26,8 @@ namespace Sitemas_de_Armas_jijija
             Console.WriteLine("Daño " + DañoEspada);
             Console.WriteLine("Velocidad de ataque " + Velocidad_Ataque_Espada);
 
-            int eldaño = Convert.ToInt32(DañoEspada / Velocidad_Ataque_Espada);      //float a int.
-
-            Console.WriteLine("Daño x segundo " + eldaño);
-            Console.WriteLine("-----------------------------");
-            //Console.WriteLine("No requiere municion, porque es un arma cuerpo a cuerpo");
+            int DañoPorSec = Convert.ToInt32(DañoEspada / Velocidad_Ataque_Espada);
+            Console.WriteLine("Daño por segundo: " + DañoPorSec);
         }
     }
 }
