eb027c5cc6c3c36d61aa7ef25b89edc04fd05215
ArchieJasper <diego_Camposdacr@hotmail.com>
2023-04-03 23:57:15 -0500

    Primer avance

---
+++ Sitemas de Armas jijija/Sitemas de Armas jijija/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sitemas_de_Armas_jijija
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            bool continueFlag = true;
+            while(continueFlag)
+            {
+                
+                string selection;
+                Console.WriteLine("Introduce un numero: ");
+                Console.WriteLine("1. Introduce el numero");
+                Console.WriteLine("2. Introduce el numero");
+                Console.WriteLine("3. Introduce el numero");
+                selection = Console.ReadLine();
+
+                switch(selection)
+                {
+                    case "1":
+                        Console.WriteLine("2. Introduce el numero");
+                        Break;
+                }
+            }
+        }
+    }
+}
